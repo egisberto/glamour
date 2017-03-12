8069aed21ced58d4b4653b109fb5eeb122848502
EGISBERTO V SILVA <egisbertovsilva@mbpdeegisberto.home>
2017-03-12 14:29:20 -0300

    Cleaning borderos files and add it to gitignore;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Form;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,9 +16,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Set all to pt_BR
+        setlocale (LC_ALL, 'pt_BR');
+        date_default_timezone_set("America/Sao_Paulo");
+        Carbon::setLocale(config('app.locale'));
+
+        //DB instructions
         Schema::defaultStringLength(191);
+
+        //Make componentes
         Form::component('clientList',       'components.clientList',        ['name', 'label', 'clients']);
-        Form::component('clientListPlus',   'components.clientListPlus',    ['name', 'label', 'clients']);
+        Form::component('clientListPlus',   'components.clientListPlus',    ['name', 'label', 'clients']);        
     }
 
     /**
